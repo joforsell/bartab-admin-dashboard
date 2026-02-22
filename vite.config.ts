04331cf300530c5b5f88964c840e5fd50dd99cd9
Johan Forsell <forsell.johan@hotmail.com>
2026-02-22 22:11:05 +0100

    Change Vite config base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import swiftWasm from "@elementary-swift/vite-plugin-swift-wasm";
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "/bartab-admin-dashboard/" : "/",
+  base: "/",
   plugins: [
     swiftWasm({
       useEmbeddedSDK: false,
