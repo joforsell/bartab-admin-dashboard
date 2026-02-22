632ee3d515f78c34292da5faa720b9e45672c67d
Johan Forsell <forsell.johan@hotmail.com>
2026-02-22 18:17:48 +0100

    Lower macOS version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "elementary-web-app",
-  platforms: [.macOS(.v26)],
+  platforms: [.macOS(.v13)],
   dependencies: [
     .package(url: "https://github.com/elementary-swift/elementary-ui.git", from: "0.1.3"),
     .package(url: "https://github.com/swiftwasm/JavaScriptKit", .upToNextMinor(from: "0.37.0")),
